6afa22aa79776c5b5b934318d80e4db978177ebe
Nicolette Chambers <nicolette3883@gmail.com>
2016-04-27 13:41:08 -0500

    Story #3 | Qwill & Nicolette | Create book class & display book metadata

---
+++ src/main/java/com/thoughtworks/tw101/biblioteca/Menu.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.tw101.biblioteca;
+
+/**
+ * Created by nicollettec on 4/27/16.
+ */
+public class Menu {
+}
---
+++ src/test/java/com/thoughtworks/tw101/biblioteca/MenuTest.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.tw101.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nicollettec on 4/27/16.
+ */
+public class MenuTest {
+
+    @Test
+
+    public st
+
+}
